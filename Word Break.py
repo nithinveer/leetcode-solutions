bc61c1c5671ae8c21408c09053588e71b204629f
Nithin Veer Reddy Kankanti <nithinveer@iitj.ac.in>
2020-08-28 18:45:51 -0600

    Update Word Break.py

--- Word Break.py
+++ Word Break.py
@@ -35,6 +35,21 @@ class Solution(object):
         self.dfs(s[0],0)
         return self.found
 
+    def wordBreakOld(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        dp = [False for i in range(len(s) + 1)]
+        dp[0] = True
+        for i in range(1, len(s) + 1):
+            for word in wordDict:
+                if dp[i - len(word)] and s[i - len(word):i] == word:
+                    dp[i] = True
+
+        return dp[-1]
+
 sol = Solution()
 s = "acaaaaabbbdbcccdcdaadcdccacbcccabbbbcdaaaaaadb"
 
