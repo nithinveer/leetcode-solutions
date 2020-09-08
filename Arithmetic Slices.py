e714f51c6c9727bd56dc4c672c20bb24090e873c
Nithin Veer Reddy Kankanti <nithinveer@iitj.ac.in>
2020-09-07 18:35:23 -0600

    Update Arithmetic Slices.py

--- Arithmetic Slices.py
+++ Arithmetic Slices.py
@@ -7,14 +7,14 @@ class Solution(object):
         rtn = 0
         count = 0
         for i in range(2, len(A)):
-            if A[i-1] - A[i-2] == A[i]- A[i-1]:
-                count +=1
+            if A[i - 1] - A[i - 2] == A[i] - A[i - 1]:
+                count += 1
             else:
                 count = 0
             rtn += count
         return rtn
 
 
-A = [1,2,3,4,5,6]
+A = [1, 2, 3, 4, 5, 6]
 sol = Solution()
 print(sol.numberOfArithmeticSlices(A))
