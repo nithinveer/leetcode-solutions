ba11dd43cc743f314fcabfa04329a802039e9b11
Nithin Veer Reddy Kankanti <nkankanti@truu.ai>
2020-10-21 23:07:37 -0600

    Update Deepest Leaves Sum.py

--- Deepest Leaves Sum.py
+++ Deepest Leaves Sum.py
@@ -29,4 +29,31 @@ class Solution(object):
         
         return sum
                 
-            
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    
+    def dfs(self, node, level):
+        if level > self.level: 
+            self.answer = node.val
+            self.level = level
+        elif level == self.level: self.answer += node.val
+        
+        if node.left : self.dfs(node.left, level+1)
+        if node.right: self.dfs(node.right, level +1)
+    
+    def deepestLeavesSum(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        self.level = self.answer = 0
+        self.dfs(root,0)
+        return self.answer
+        
